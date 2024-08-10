7cbcaf8d68d7e4967356cf5a3c5fc84d5f6727af
kanhucharansahoo595 <kanhucharansahoo595@gmail.com>
2024-08-10 20:17:43 +0000

    create the folder structure of the backend application.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({});
+const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
-});
-
-const port = parseInt(process.env.PORT) || 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
